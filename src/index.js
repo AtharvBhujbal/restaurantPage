cbabcbf2ac0276e17ab889086ca9f21bf4b0f953
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2024-05-29 16:56:32 +0530

    Updated style.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import createHome from './home';
 import createAbout from './about';
 import createMenu from './menu';
+import './style.css';
 // console.log("Hello There,My name is Atharv");
+
 const homebtn = document.getElementById('home');
 const aboutbtn = document.getElementById('about');
 const menubtn = document.getElementById('menu');
@@ -25,3 +27,12 @@ homebtn.addEventListener("click", btnCall);
 aboutbtn.addEventListener("click", btnCall);
 menubtn.addEventListener("click", btnCall);
 homebtn.click();
+
+function createHeader(){
+    const header = document.getElementById('header');
+    const navbar = document.getElementById('navbar');
+    const resturantName = document.createElement('h1');
+    resturantName.textContent = "Chicken Mania";
+    header.insertBefore(resturantName,navbar);
+}
+createHeader();
